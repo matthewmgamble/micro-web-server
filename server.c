57e06aff98430285f9725816e31c3d8b75b94c43
Matthew Gamble <mgamble@mgamble.ca>
2017-05-18 12:27:05 -0400

    Update server.c
    Changing to loopback only (because that's what we need ) - will do a future change to make this a command line option.

--- server.c
+++ server.c
@@ -577,7 +577,7 @@ int main (int argc, char ** argv) {
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
 	servaddr.sin_port   = htons(port);
 	int yes = 1;
 	setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
